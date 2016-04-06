68a61c076bed251a431cce9a74ec3adf7d2ca69d
grv <andreasgrv@gmail.com>
2016-04-06 04:20:32 +0300

    docstr + raise error for unimplemented options

--- pytlgu/__init__.pyx
+++ pytlgu/__init__.pyx
@@ -1,5 +1,9 @@
+"""python interface to the tlgu. currently only exposes the convert function
+which is the interface tlgu exposes at the command line."""
 STR_SIZE = 252
 
+# hold your breath, this list is long
+# TODO check if there is a clearer way to do this
 def convert(in_filename,
             out_filename,
             roman=False,
@@ -27,13 +31,20 @@ def convert(in_filename,
             nospace=False,
             cformat='',
             ecite=''):
-    """call C tlgu after setting global variables to values passed"""
+    """writes utf-8 encoded output corresponding to input using tlgu for the
+    conversion. This is done by calling the C tlgu function after setting
+    global variables to the values passed.
+    :in_filename: the filename to read input encoding from
+    :out_filename: the filename to write utf-8 output to
+    :...: all the rest of the arguments which I can't describe at the moment
+    """
+    if cformat or ecite:
+        raise NotImplemented('Not supporting arguments ecite and cformat yet')
     # This is extremely ugly, but couldn't script it differently
     # there seems to be no support for global() in cython
     # if it did we could do :
     # for arg in args:
     #     globals()[arg] = locals()[arg.replace('opt_', '')]
-    # TODO check if there is a clearer way to do this (maybe with a class?)
     global roman, page, blank, acit, bcit, ccit, dcit, cit_id, vcit, wcit
     global xcit, ycit, cprefix, ecit_blank, zcit, verbose, debug_bracket
     global debug_cit, debug_special, multiple, ebook_cit_x, ebook_cit_y
@@ -61,6 +72,7 @@ def convert(in_filename,
     opt_ebook_cit_x = ebook_cit_x
     opt_ebook_cit_y = ebook_cit_y
     opt_nospace = nospace
+    # TODO support below arguments which accept a string as argument
     # cformat = bytes(cformat[:STR_SIZE])
     # ecite = bytes(ecite[:STR_SIZE])
     bytes_in_filename = in_filename.encode('utf-8')
